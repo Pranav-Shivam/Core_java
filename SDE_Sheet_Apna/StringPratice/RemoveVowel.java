81c1e46c14a4bf327c1946b8f74404be03ee1163
Pranav-Shivam <21MCA2618@cuchd.in>
2022-08-15 00:51:57 +0530

    ADD ON

---
+++ SDE_Sheet_Apna/StringPratice/RemoveVowel.java
@@ -0,0 +1,12 @@
+package SDE_Sheet_Apna.StringPratice;
+
+public class RemoveVowel {
+    public static void main(String[] args) {
+        String S="welcome to geeksforgeeks";
+        String[] ls=new String[]{"a","e","i","o","u"};
+        for(int i=0;i<ls.length;i++){
+            S=S.replaceAll(ls[i],"");
+        }
+        System.out.println(S);
+    }
+}
