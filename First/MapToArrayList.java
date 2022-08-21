c04e5afac75b79bfad44434f27b86383c29e7bca
Pranav-Shivam <21MCA2618@cuchd.in>
2022-08-21 16:46:36 +0530

    Map adder

--- First/MapToArrayList.java
+++ First/MapToArrayList.java
@@ -8,6 +8,7 @@ public class MapToArrayList {
         map.put("Pranav",1000);
         map.put("Toamr",5000);
         map.put("Shivam",6000);
+        System.out.println(map);
         Iterator it=map.entrySet().iterator();
         while (it.hasNext())
         {
@@ -23,5 +24,11 @@ public class MapToArrayList {
         ArrayList<Integer> val=new ArrayList<>(map.values());
         System.out.println(name);
         System.out.println(val);
+        LinkedHashMap<String,Integer> map1=new LinkedHashMap<>();
+        map1.put("Pranav",1000);
+        map1.put("Toamr",5000);
+        map1.put("Shivam",6000);
+        System.out.println(map1);
+
     }
 }
