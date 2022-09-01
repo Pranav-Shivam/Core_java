11a6c481854aba14fe38e23d4c5c17abde8548a6
Pranav_Shivam <21MCA2618@cuchd.in>
2022-09-02 01:33:45 +0530

    Checking

--- DSA/BinarySerachRecursion.java
+++ DSA/BinarySerachRecursion.java
@@ -3,7 +3,7 @@ package DSA;
 public class BinarySerachRecursion {
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5,6,7,8,9};
-        int k=10;
+        int k=6;
         int index=binarySerach(arr,k,0, arr.length-1);
         System.out.println(index);
     }
